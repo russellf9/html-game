25cec08aaafe21c1371f9c556a4da20af9706edd
russellf9 <russell@factornine.co.uk>
2015-02-03 14:27:52 +0000

    * Coding standards - Added and implemented HINT and JSCS.

--- _js/game.js
+++ _js/game.js
@@ -1,45 +1,39 @@
+'use strict';
+
 var BubbleShoot = window.BubbleShoot || {};
 
 BubbleShoot.Game = (function($) {
-	var Game = function() {
-		var curBubble;
-		this.init = function() {
-			console.log('init!');
-			$('.but_start_game').bind('click', startGame);
-		};
-		var startGame = function() {
-			$('.but_start_game').unbind('click');
-			BubbleShoot.ui.hideDialog();
-			curBubble = getNextBubble();
-			$('#game').bind('click',clickGameScreen);
-		};
-		var getNextBubble = function() {
-			var bubble = BubbleShoot.Bubble.create();
-			bubble.getSprite().addClass('cur_bubble');
-			$('#board').append(bubble.getSprite());
-			return bubble;
-		};
-		var clickGameScreen = function(event){
-			var angle = BubbleShoot.ui.getBubbleAngle(curBubble.getSprite(),event);
-
-			console.log('Game::clickGameScreen - angle ', angle);
-
-			var duration = 750;
-			var distance = 1000;
-			var distX = Math.sin(angle) * distance;
-			var distY = Math.cos(angle) * distance;
-			var bubbleCoords = BubbleShoot.ui.getBubbleCoords(curBubble.getSprite());
-			var coords = {
-				x : bubbleCoords.left + distX,
-				y : bubbleCoords.top - distY
-			};
-			BubbleShoot.ui.fireBubble(curBubble, coords, duration);
-
-
-
-
-
-		};
-	};
-	return Game;
+    var Game = function() {
+        var curBubble;
+        this.init = function() {
+            console.log('init!');
+            $('.but_start_game').bind('click', startGame);
+        };
+        var startGame = function() {
+                $('.but_start_game').unbind('click');
+                BubbleShoot.ui.hideDialog();
+                curBubble = getNextBubble();
+                $('#game').bind('click', clickGameScreen);
+            },
+            getNextBubble = function() {
+                var bubble = BubbleShoot.Bubble.create();
+                bubble.getSprite().addClass('cur_bubble');
+                $('#board').append(bubble.getSprite());
+                return bubble;
+            },
+            clickGameScreen = function(event) {
+                var angle = BubbleShoot.ui.getBubbleAngle(curBubble.getSprite(), event),
+                duration = 750,
+                    distance = 1000,
+                    distX = Math.sin(angle) * distance,
+                    distY = Math.cos(angle) * distance,
+                    bubbleCoords = BubbleShoot.ui.getBubbleCoords(curBubble.getSprite()),
+                    coords = {
+                        x: bubbleCoords.left + distX,
+                        y: bubbleCoords.top - distY
+                    };
+                BubbleShoot.ui.fireBubble(curBubble, coords, duration);
+            };
+    };
+    return Game;
 })(jQuery);
